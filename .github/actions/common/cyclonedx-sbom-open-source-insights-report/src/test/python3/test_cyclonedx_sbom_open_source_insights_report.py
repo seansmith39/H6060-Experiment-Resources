145f225feb99ab5d1f00a3001c82f65e70d37ac1
sean.smith <sean.smith@workday.com>
2024-07-14 16:14:09 +0100

    Create action.yml

--- .github/actions/common/cyclonedx-sbom-open-source-insights-report/src/test/python3/test_cyclonedx_sbom_open_source_insights_report.py
+++ .github/actions/common/cyclonedx-sbom-open-source-insights-report/src/test/python3/test_cyclonedx_sbom_open_source_insights_report.py
@@ -7,14 +7,14 @@ import logging
 import unittest
 from argparse import Namespace
 from unittest.mock import patch
-from main.python3 import sbom_open_source_insights_report
+from main.python3 import cyclonedx_sbom_open_source_insights_report
 
 
 TEST_DIRECTORY_RESOURCES = os.path.dirname(os.path.realpath(__file__)) + "/resources/"
 
-CSV_JAVA_RESULT_FILENAME = "sbom_open_source_insights_java_report.csv"
-CSV_JAVASCRIPT_RESULT_FILENAME = "sbom_open_source_insights_javascript_report.csv"
-CSV_PYTHON_RESULT_FILENAME = "sbom_open_source_insights_python_report.csv"
+CSV_JAVA_RESULT_FILENAME = "cyclonedx_sbom_open_source_insights_java_report.csv"
+CSV_JAVASCRIPT_RESULT_FILENAME = "cyclonedx_sbom_open_source_insights_javascript_report.csv"
+CSV_PYTHON_RESULT_FILENAME = "cyclonedx_sbom_open_source_insights_python_report.csv"
 
 CYCLONEDX_SBOM_JAVA_FILENAME = TEST_DIRECTORY_RESOURCES + "sbom/java/cyclonedx_sbom_report.json"
 CYCLONEDX_SBOM_JAVA_MISCONFIGURED_FILENAME = (
@@ -90,8 +90,8 @@ class TestSbomOpenSourceInsightsReport(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        sbom_open_source_insights_report.log = logging.getLogger()
-        sbom_open_source_insights_report.log.setLevel(logging.INFO)
+        cyclonedx_sbom_open_source_insights_report.log = logging.getLogger()
+        cyclonedx_sbom_open_source_insights_report.log.setLevel(logging.INFO)
         with open(os.devnull, "w") as f:
             sys.stdout = f
 
@@ -111,11 +111,11 @@ class TestSbomOpenSourceInsightsReport(unittest.TestCase):
         )
 
     @patch(
-        "main.python3.sbom_open_source_insights_report.send_post_request",
+        "main.python3.cyclonedx_sbom_open_source_insights_report.send_post_request",
         side_effect=mocked_response,
     )
     def test_query_osv_api(self, mock_response):
-        osv_data = sbom_open_source_insights_report.query_osv_api([], "pkg:pypi/django@5.0", "5.0")
+        osv_data = cyclonedx_sbom_open_source_insights_report.query_osv_api([], "pkg:pypi/django@5.0", "5.0")
         self.assertTrue(len(osv_data) > 0)
         self.assertEqual(osv_data[0][0], "GHSA-9jmf-237g-qf46")
         self.assertEqual(osv_data[0][1], "Django Path Traversal vulnerability")
@@ -131,76 +131,61 @@ class TestSbomOpenSourceInsightsReport(unittest.TestCase):
         self.assertEqual(osv_data[0][11], "CVSS:4.0/AV:N/AC:L/AT:N/PR:N/UI:N/VC:H/VI:N/VA:N/SC:N/SI:N/SA:N")
 
     @patch(
-        "main.python3.sbom_open_source_insights_report.send_post_request",
+        "main.python3.cyclonedx_sbom_open_source_insights_report.send_post_request",
         side_effect=mocked_response,
     )
     def test_query_osv_api_no_vulnerabilities(self, mock_response):
-        osv_data = sbom_open_source_insights_report.query_osv_api([], "no_vulns", "1.0")
+        osv_data = cyclonedx_sbom_open_source_insights_report.query_osv_api([], "no_vulns", "1.0")
         self.assertEqual(osv_data, [None])
 
     @patch(
-        "main.python3.sbom_open_source_insights_report.send_post_request",
+        "main.python3.cyclonedx_sbom_open_source_insights_report.send_post_request",
         side_effect=mocked_response,
     )
     def test_query_osv_api_no_records(self, mock_response):
-        osv_data = sbom_open_source_insights_report.query_osv_api([], "no_record", "1.0")
+        osv_data = cyclonedx_sbom_open_source_insights_report.query_osv_api([], "no_record", "1.0")
         self.assertEqual(osv_data, [None])
 
     def test_query_osv_api_no_response(self):
-        osv_data = sbom_open_source_insights_report.query_osv_api([], "no_response", "1.0")
+        osv_data = cyclonedx_sbom_open_source_insights_report.query_osv_api([], "no_response", "1.0")
         self.assertEqual(osv_data, [None])
 
     def test_invalid_programming_language(self):
         args = self.__mock_args("invalid", CYCLONEDX_SBOM_PYTHON_FILENAME)
         with self.assertRaises(SystemExit) as cm:
-            sbom_open_source_insights_report.main(args)
+            cyclonedx_sbom_open_source_insights_report.main(args)
         self.assertEqual(cm.exception.code, 1)
-
-    def test_get_csv_column_headers_missing_programming_language(self):
-        result = sbom_open_source_insights_report.get_csv_column_headers("missing_language")
-        default_headers = [
-            "OSV Vulnerability ID",
-            "OSV Vulnerability Summary",
-            "OSV Vulnerability CVE",
-            "OSV Vulnerability Severity",
-            "OSV Vulnerability CWE IDs",
-            "OSV Vulnerability NVD Published Date",
-            "OSV Vulnerability Advisory URL",
-            "OSV Vulnerability Introduced",
-            "OSV Vulnerability Fixed",
-            "OSV Vulnerability CVSS V2",
-            "OSV Vulnerability CVSS V3",
-            "OSV Vulnerability CVSS V4",
-        ]
-
-        self.assertEqual(result, "," + ",".join(default_headers) + "\n")
 
     def test_get_json_value_missing_key(self):
         with open(RESPONSE_OSV_API_JAVA, "r") as f:
             osv_data = JSON.load(f)
-        result = sbom_open_source_insights_report.get_json_value(osv_data["vulns"][0], "published", "missing_key")
+        result = cyclonedx_sbom_open_source_insights_report.get_json_value(
+            osv_data["vulns"][0], "published", "missing_key"
+        )
         self.assertEqual(result, "N/A")
 
     def test_get_vulnerability_cve(self):
         with open(RESPONSE_OSV_API_JAVA, "r") as f:
             osv_data = JSON.load(f)
-        result = sbom_open_source_insights_report.get_vulnerability_cve(osv_data["vulns"][0])
+        result = cyclonedx_sbom_open_source_insights_report.get_vulnerability_cve(osv_data["vulns"][0])
         self.assertEqual(result, "CVE-2024-36401,CVE-2024-36404")
 
     def test_get_vulnerability_cve_not_found(self):
         with open(RESPONSE_OSV_API_PYTHON, "r") as f:
             osv_data = JSON.load(f)
-        result = sbom_open_source_insights_report.get_vulnerability_cve(osv_data["vulns"][1])
+        result = cyclonedx_sbom_open_source_insights_report.get_vulnerability_cve(osv_data["vulns"][1])
         self.assertEqual(result, "N/A")
 
     def test_get_vulnerability_affected_version_not_found(self):
         with open(RESPONSE_OSV_API_PYTHON, "r") as f:
             osv_data = JSON.load(f)
-        result = sbom_open_source_insights_report.get_vulnerability_affected_version(osv_data["vulns"][1], "1.0")
+        result = cyclonedx_sbom_open_source_insights_report.get_vulnerability_affected_version(
+            osv_data["vulns"][1], "1.0"
+        )
         self.assertEqual(result, ("N/A", "N/A"))
 
     def test_java_misconfigured_main(self):
-        args = sbom_open_source_insights_report.get_args(
+        args = cyclonedx_sbom_open_source_insights_report.get_args(
             [
                 "--programming-language",
                 "java",
@@ -209,11 +194,11 @@ class TestSbomOpenSourceInsightsReport(unittest.TestCase):
             ]
         )
         with self.assertRaises(SystemExit) as cm:
-            sbom_open_source_insights_report.main(args)
+            cyclonedx_sbom_open_source_insights_report.main(args)
         self.assertEqual(cm.exception.code, 1)
 
     def test_python_misconfigured_main(self):
-        args = sbom_open_source_insights_report.get_args(
+        args = cyclonedx_sbom_open_source_insights_report.get_args(
             [
                 "--programming-language",
                 "python",
@@ -222,11 +207,11 @@ class TestSbomOpenSourceInsightsReport(unittest.TestCase):
             ]
         )
         with self.assertRaises(SystemExit) as cm:
-            sbom_open_source_insights_report.main(args)
+            cyclonedx_sbom_open_source_insights_report.main(args)
         self.assertEqual(cm.exception.code, 1)
 
     def test_javascript_misconfigured_main(self):
-        args = sbom_open_source_insights_report.get_args(
+        args = cyclonedx_sbom_open_source_insights_report.get_args(
             [
                 "--programming-language",
                 "javascript",
@@ -235,15 +220,15 @@ class TestSbomOpenSourceInsightsReport(unittest.TestCase):
             ]
         )
         with self.assertRaises(SystemExit) as cm:
-            sbom_open_source_insights_report.main(args)
+            cyclonedx_sbom_open_source_insights_report.main(args)
         self.assertEqual(cm.exception.code, 1)
 
     @patch(
-        "main.python3.sbom_open_source_insights_report.send_post_request",
+        "main.python3.cyclonedx_sbom_open_source_insights_report.send_post_request",
         side_effect=mocked_response,
     )
     def test_java_main(self, mock_response):
-        args = sbom_open_source_insights_report.get_args(
+        args = cyclonedx_sbom_open_source_insights_report.get_args(
             [
                 "--programming-language",
                 "java",
@@ -251,15 +236,15 @@ class TestSbomOpenSourceInsightsReport(unittest.TestCase):
                 CYCLONEDX_SBOM_JAVA_FILENAME,
             ]
         )
-        result = sbom_open_source_insights_report.main(args)
+        result = cyclonedx_sbom_open_source_insights_report.main(args)
         self.assertEqual(result, None)
 
     @patch(
-        "main.python3.sbom_open_source_insights_report.send_post_request",
+        "main.python3.cyclonedx_sbom_open_source_insights_report.send_post_request",
         side_effect=mocked_response,
     )
     def test_python_main(self, mock_response):
-        args = sbom_open_source_insights_report.get_args(
+        args = cyclonedx_sbom_open_source_insights_report.get_args(
             [
                 "--programming-language",
                 "python",
@@ -267,15 +252,15 @@ class TestSbomOpenSourceInsightsReport(unittest.TestCase):
                 CYCLONEDX_SBOM_PYTHON_FILENAME,
             ]
         )
-        result = sbom_open_source_insights_report.main(args)
+        result = cyclonedx_sbom_open_source_insights_report.main(args)
         self.assertEqual(result, None)
 
     @patch(
-        "main.python3.sbom_open_source_insights_report.send_post_request",
+        "main.python3.cyclonedx_sbom_open_source_insights_report.send_post_request",
         side_effect=mocked_response,
     )
     def test_javascript_main(self, mock_response):
-        args = sbom_open_source_insights_report.get_args(
+        args = cyclonedx_sbom_open_source_insights_report.get_args(
             [
                 "--programming-language",
                 "javascript",
@@ -283,7 +268,7 @@ class TestSbomOpenSourceInsightsReport(unittest.TestCase):
                 CYCLONEDX_SBOM_JAVASCRIPT_FILENAME,
             ]
         )
-        result = sbom_open_source_insights_report.main(args)
+        result = cyclonedx_sbom_open_source_insights_report.main(args)
         self.assertEqual(result, None)
 
     @classmethod
